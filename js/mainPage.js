c9a03601f92c3068f9c3dc8a444f1e3dca2ed13b
limengyi <mli185@student.monash.edu>
2017-05-11 22:51:51 +1000

    Main page editing
    finishing the main page edit

--- js/mainPage.js
+++ js/mainPage.js
@@ -10,3 +10,24 @@ function viewLocation(locationName)
     // And load the view location page.
     location.href = 'viewlocation.html';
 }
+var weatherUtil=loadLocations();
+var date=new Date();
+var str="";
+//alert(weatherUtil.length());
+for (var i=0; i<weatherUtil.length();i++){
+    //if (weatherUtil.locationAtIndex(i).forecasts===null){
+    weatherUtil.getWeatherAtIndexForDate(i,date.darkSkyDateString(),function(){
+        
+    });
+//}
+   var summary="";
+		if(weatherUtil.locationAtIndex(i).forecasts != null)
+			summary = weatherUtil.locationAtIndex(i).forecasts.value.daily.data.summary;
+	str += '<li class="mdl-list__item mdl-list__item--two-line" onclick="viewLocation('+i+');">'
+              +  '<span class="mdl-list__item-primary-content">'
+               +   '<img class="mdl-list__item-icon" id="icon0" src="images/loading.png" class="list-avatar" />'
+                +  '<span>'+ summary +'</span>'
+                +  '<span id="weather'+i+'" class="mdl-list__item-sub-title">'+weatherUtil.locationAtIndex(i).nickname+'</span></span></li>'
+}
+
+document.getElementById("locationList").innerHTML = str;
