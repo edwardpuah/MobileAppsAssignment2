3ea6512ebfc09e42aaaa5101aa0735b664f59f8f
Han2333 <hluo0002@student.monash.edu>
2017-05-13 02:28:41 +1000

    Update addLocationPage.js

--- js/addLocationPage.js
+++ js/addLocationPage.js
@@ -1,29 +1,76 @@
 // Code for the Add Location page.
-var map;
-var result;
-function initMap(){
-    map=new google.maps.Map(document.getElementById('map'),{
-        center:{lat:-34.397,lng:150.644},
-        zoom:8
-    });
-}
-function getGeoByAddress(){
-    var geocoder=new google.maps.Geocoder();
-    var address=document.getElementById('address').value;
-    geocoder.geocode({'address':address}, function(results, status){
-        if (status==='OK') {
-            result=result[0];
-            map.setCenter(results[0].geometry.location);
-            var marker=new google.maps.Maker({
-                map:map,
-                position:result[0].geometry.loation,
-                label:{
-                    text:results[0].formatted_address,
-                    color:'red'
-                }
-            });
-        }else{
-            
-        }
-    });
-}
+
+//inital Map
+var map = null
+  function initMap() 
+        {
+// Initialise map, centred on Melbourne, Australia.
+            map = new google.maps.Map(document.getElementById('map'), 
+                                      {
+                                            center: {
+                                                lat: -37.8200855,
+                                                lng: 144.9608045
+                                                    },
+                                            zoom: 17
+                                      }
+                                      );
+        
+// Create the search box and link it to the UI element.
+        var input = document.getElementById('newAddress');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+// Bias the SearchBox results towards current map's viewport.
+        map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+        });
+
+        var markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+
+          // Clear out the old markers.
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = [];
+
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+        });
+      }
+      //from https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
